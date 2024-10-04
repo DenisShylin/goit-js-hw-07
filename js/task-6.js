beb20649ff559cbaf843ed9999d851ef96cdcb02
Shylin Denis <tawnship406@gmail.com>
2024-10-04 12:05:47 +0300

    Start

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,45 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const refs = {
+  input: document.querySelector("#controls input"),
+  createBtn: document.querySelector("[data-create]"),
+  destroyBtn: document.querySelector("[data-destroy]"),
+  boxesContainer: document.getElementById("boxes"),
+};
+
+refs.createBtn.addEventListener("click", onCreateBtnClick);
+refs.destroyBtn.addEventListener("click", onDestroyBtnClick);
+
+function onCreateBtnClick() {
+  const amount = Number(refs.input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    refs.input.value = "";
+  }
+}
+
+function onDestroyBtnClick() {
+  destroyBoxes();
+}
+
+function createBoxes(amount) {
+  destroyBoxes();
+
+  const boxes = [];
+  let size = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+    size += 10;
+  }
+
+  refs.boxesContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  refs.boxesContainer.innerHTML = "";
+}
